6ec2fe0e13f6f744eeb77c4222e4b8b6291e55b2
Branden Howard <hungryheadcrab@gmail.com>
2018-05-30 17:34:47 -0700

    prices

--- RowGuys/Artifact/Pretty Rock.gd
+++ RowGuys/Artifact/Pretty Rock.gd
@@ -4,6 +4,8 @@ extends "res://Artifact/Artifact.gd"
 func _ready():
 	stats.Vitality = 1
 	description = "Pretty Rock: Grants regen 1 and increases vitality by 1 for the whole party"
+	buyPrice = 5
+	sellPrice = 2
 
 
 func Upkeep(unit):
